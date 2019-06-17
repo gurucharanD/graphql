89b8e9baef03bd2d3adc48e24892d503baa23d02
gurucharanD <gurucharandandyala@gmail.com>
2019-06-17 23:39:54 +0530

    relations added

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -25,18 +25,45 @@ const posts=[
         id: 1,
         title:'post1' ,
         body: 'body of post1',
-        isPublished: true
+        isPublished: true,
+        author:123
     },{
         id: 2,
         title:'post2' ,
         body: 'body of post 2',
-        isPublished: false
+        isPublished: false,
+        author:122
     },{
         id: 3,
         title:'post3' ,
         body: 'body of post3',
-        isPublished: true
+        isPublished: true,
+        author:121
     }
+]
+
+const comments=[
+    {
+    id:1,
+    text:'super',
+    author:123,
+    post:1
+},{
+   id:2,
+   text: 'ok',
+   author:122,
+   post:2
+},{
+    id:3,
+    text:'bad',
+    author:121,
+    post:3
+},{
+    id:4,
+    text:'worst',
+    author:122,
+    post:2
+}
 ]
 
 const typeDefs=`
@@ -44,6 +71,14 @@ type Query{
     me: User!
     post(query: String): [Post!]!
     users(query: String): [User!]!
+    comments : [Comment!]!
+}
+
+type Comment{
+    id: ID!
+    text: String!
+    author: User!
+    post: Post!
 }
 
 type User{
@@ -51,6 +86,8 @@ type User{
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
+    comments: [Comment!]!
 }
 
 type Post{
@@ -58,6 +95,8 @@ type Post{
     title: String!
     body: String!
     isPublished: Boolean
+    author: User!
+    comments: [Comment!]!
 }
 `
 const resolvers={
@@ -88,9 +127,36 @@ const resolvers={
                 return users.filter(user=>user.name.toLowerCase().includes(args.query.toLowerCase()))
             }
               return users
+        },
+        comments(parent,args,ctx,info){
+            return comments
         }
+    },
+    Post:{
+        author(parent,args,ctx,info){
+          return users.find(user=>user.id==parent.author)
+        },
+        comments(parent,args,ctx,info){
+            return comments.filter(comment=>comment.id==parent.id)
+          }
+    },
+    User:{
+        posts(parent,args,ctx,info){
+          return posts.filter(post=>post.author==parent.id)
+        },
+        comments(parent,args,ctx,info){
+          return comments.filter(comment=>comment.author==parent.id)
+        }
+    },
+    Comment:{
+        author(parent,args,ctx,info){
+          return users.find(user=>user.id==parent.author)
+        },
+        post(parent,args,ctx,info){
+            return posts.find(post=>post.id==parent.post)
+          }
     }
-    }
+}
 
 
 const server=new GraphQLServer({
